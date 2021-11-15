bdfd61e461f0126ea3e117213ab9f76482804ec9
alexa <alexalfi00@gmail.com>
2021-11-14 21:20:31 -0600

    #14 conexion de vista con funcionalidades

--- src/app/micalculadora/micalculadora.component.ts
+++ src/app/micalculadora/micalculadora.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { calcularAreaBase } from '../c-area-base/c-area-base.component';
+import { calcularAreaCara } from '../calcularAreaCara/calcularAreaCara';
+import { calcularAreaFinal } from '../cAreaFinal/calcularAreaFinal';
+import { calcularAreaSuperior } from '../cAreaSuperior/calcular-area-superior';
 
 @Component({
   selector: 'app-micalculadora',
@@ -12,7 +16,27 @@ export class MicalculadoraComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  base = 0;
+  altura = 0;
+
   resultado = 100;
   titulo = "Calcular area de una piramide cuadrangular"
+  hola(){
+    alert("Hola mundo")
+  }
+
+  obtenerArea(){
+    // Todo funciona, bien, pero, me confundí en la formula del area superior :v
+
+    let areaBase = calcularAreaBase(this.base);
+    alert(areaBase);
+
+    let areaSuperior = calcularAreaSuperior(calcularAreaCara(this.base, this.altura));
+
+    let areaTotal = calcularAreaFinal(areaBase, areaSuperior);
+
+    this.resultado = areaTotal;
+  }
 
 }
